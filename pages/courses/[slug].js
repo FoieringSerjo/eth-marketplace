e5f504dbd309e60ef5c66db546aae9bfa7db2d24
Foiering Sergey <serjofoi@gmail.com>
2022-12-04 17:24:06 +0200

    Course detail page + Get single course

---
+++ pages/courses/[slug].js
@@ -0,0 +1,42 @@
+import { Modal } from '@components/common';
+import { CourseHero, Curriculum, Keypoints } from '@components/course';
+import { BaseLayout } from '@components/layout';
+import { getAllCourses } from '@content/courses/fetcher';
+
+export default function Course({ course }) {
+  return (
+    <BaseLayout>
+      {course.title}
+      <div className="py-4">
+        <CourseHero />
+      </div>
+      <Keypoints />
+      <Curriculum />
+      <Modal />
+    </BaseLayout>
+  );
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses();
+
+  return {
+    paths: data.map((course) => ({
+      params: {
+        slug: course.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  const { data } = getAllCourses();
+
+  const course = data.filter((course) => course.slug === params.slug)[0];
+  return {
+    props: {
+      course,
+    },
+  };
+}
