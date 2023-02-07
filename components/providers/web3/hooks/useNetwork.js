374597d82e797b197877f58d368987c6a944c261
FoieringSerjo <serjofoi@gmail.com>
2023-02-07 11:04:25 +0200

    Dynamic ChainID

--- components/providers/web3/hooks/useNetwork.js
+++ components/providers/web3/hooks/useNetwork.js
@@ -1,17 +1,27 @@
 import { useEffect } from 'react';
 import useSWR from 'swr';
 
+const NETWORKS = {
+  1: 'Ethereum Main Network',
+  5: 'Ethereum Goerli Network',
+  56: 'Binance Smart Chain',
+  11155111: 'Ethereum Sepolia Network',
+};
+
 export const handler = (web3, provider) => () => {
   const { mutate, ...restSWRResponse } = useSWR(
     () => (web3 ? 'web3/network' : null),
     async () => {
-      const netId = await web3.eth.net.getId();
-      return netId;
+      const chainId = await web3.eth.getChainId();
+      return NETWORKS[chainId];
     }
   );
 
   useEffect(() => {
-    provider && provider.on('chainChanged', (netId) => mutate(netId));
+    provider &&
+      provider.on('chainChanged', (chainId) => {
+        mutate(NETWORKS[parseInt(chainId, 16)]);
+      });
   }, [web3]);
 
   return {
