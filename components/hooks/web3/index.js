662e5f137ac8ef0ed92d4adbcb5bf5307e1673d8
Foiering Sergey <serjofoi@gmail.com>
2023-03-03 22:28:34 +0200

    Refactor hooks, useWalletInfo

--- components/hooks/web3/index.js
+++ components/hooks/web3/index.js
@@ -20,3 +20,16 @@ export const useAccount = () => {
     account: swrRes,
   };
 };
+
+export const useWalletInfo = () => {
+  const { account } = useAccount();
+  const { network } = useNetwork();
+
+  //Note - null && true = null, !!(null && true) = false
+  const canPurchaseCourse = !!(account.data && network.isSupported);
+  return {
+    account,
+    network,
+    canPurchaseCourse,
+  };
+};
